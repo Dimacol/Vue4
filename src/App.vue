002e1b5ae730edb737cd480cadbcce601fd22ad3
Dima Kulushev <dmitrijkulushev@gmail.com>
2023-09-03 13:44:39 +0300

    HomeWork4

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <Header />
+  <HomeMain />
+  <BlogMain />
+  <BlogDetailsMain />
+  <Project />
+  <Footer />
+</template>
+
+<script>
+
+import Header from './components/Header.vue';
+import HomeMain from './components/HomeMain.vue';
+import BlogMain from './components/BlogMain.vue';
+import BlogDetailsMain from './components/BlogDetailsMain.vue';
+import Project from './components/Project.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    HomeMain,
+    BlogMain,
+    BlogDetailsMain,
+    Project,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  font-family: 'DM Serif Display', serif;
+  font-family: 'Jost', sans-serif;
+  background-color: #fff;
+}
+
+a {
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+}
+
+.center {
+  padding-left: calc(50% - 600px);
+  padding-right: calc(50% - 600px);
+}
+</style>
---
+++ src/components/BlogIntro.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="intro"> 
+        <IntroText class="intro__title" :text="text" v-for="text in blogTexts" />
+    </section>
+</template>
+
+<script>
+import IntroText from './IntroText.vue';
+
+export default {
+    name: 'WebsiteInternoVueSliBlogIntro',
+    data() {
+        return {
+            blogTexts: [
+                {
+                    id: "blogText",
+                    title: "Articles & News",
+                    subheadOne: "Home",
+                    subheadTwo: "Blog",
+                    routerlink: "/blog",
+                },
+            ],
+        };
+    },
+    mounted() {
+    },
+    methods: {},
+    components: { IntroText }
+};
+</script>
+
+<style lang="scss" scoped>
+.intro {
+    margin-top: 45px;
+    background-image: url("../assets/blog_intro-picture.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+    display: flex;
+    justify-content: center;
+
+    &__title {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 40px 78px;
+        width: 503px;
+        margin-top: 179px;
+        border-radius: 37px 37px 0px 0px;
+        background-color: #fff;
+    }
+}
+</style>
